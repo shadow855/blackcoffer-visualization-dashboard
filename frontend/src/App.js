f8c61f68f3f005931032fdc7d1a1808a484d6e8c
shadow855 <raiyanmushtaque@gmail.com>
2024-07-13 19:52:09 +0530

    added url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Dashboard from './Components/Dashboard';
 
+export const URL = process.env.REACT_APP_SERVER_URL;
+
 function App() {
   return (
     <Dashboard />
--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -36,7 +36,7 @@ const Dashboard = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('http://localhost:5000/api/data');
+                const response = await axios.get('/api/data');
                 setData(response.data);
             } catch (error) {
                 console.error('Error fetching data:', error);
