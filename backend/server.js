b70b8c437ccb03932fb944590d526d06577919ad
shadow855 <raiyanmushtaque@gmail.com>
2024-07-13 19:58:50 +0530

    added deployed url cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ["http://localhost:3000", ""]
+    origin: ["http://localhost:3000", "https://blackcoffer-visualization-dashboard-oigr.onrender.com"]
 }));
 
 
